4e234e504744c1955e892a5d356512dd00abcd69
Ma Xueyuan <machinemxy@me.com>
2021-01-24 22:46:58 +0900

    add project and images

---
+++ Mahjong/MahjongApp.swift
@@ -0,0 +1,17 @@
+//
+//  MahjongApp.swift
+//  Mahjong
+//
+//  Created by 馬学渊 on 2021/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MahjongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
