45a913d8e07598fed6fcb47b6992ea4b12d2e60a
Ma Xueyuan <machinemxy@me.com>
2021-02-15 23:08:40 +0900

    Made a segment control to switch from all yakus and open yakus.

--- Mahjong/ContentView.swift
+++ Mahjong/ContentView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var yakus: [Yaku] = Bundle.main.decode("yaku_en.json")
+    let yakus: [Yaku] = Bundle.main.decode("yaku_en.json")
+    let options = ["All yakus", "Open yakus"]
+    @State private var selectedOption = 0
+    
+    var selectedYakus: [Yaku] {
+        if selectedOption == 0 {
+            return yakus
+        } else {
+            return yakus.filter { $0.marks.count > 1 }
+        }
+    }
     
     var body: some View {
         NavigationView {
-            List(yakus, id: \.title) { yaku in
-                YakuView(yaku: yaku)
+            List {
+                Picker("Options", selection: $selectedOption) {
+                    ForEach(0..<2) { index in
+                        Text(options[index]).tag(index)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                
+                ForEach(selectedYakus, id: \.title) { yaku in
+                    YakuView(yaku: yaku)
+                }
             }
             .navigationBarTitle("Yakus")
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
